8db39e916a6b805b0488a7ddb02238a2b1a835c9
Carlos Motta <motta.carlos08@gmail.com>
2019-10-31 17:44:43 -0300

    first commit

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA6O3MIUTVGW4TKTUI"
-  secret_key = "mYlvQfk+ExrtfkIRXhvIdB2jVfVHlavLdiL5Ou5P"
+  access_key = "<your_acces_key>"
+  secret_key = "<your_secret_key>"
 }
