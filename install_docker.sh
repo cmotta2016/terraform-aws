737301b420cc288f80266a0ea540c5f54a74dae1
Carlos Motta <motta.carlos08@gmail.com>
2019-11-01 12:11:38 -0300

    first commit

---
+++ install_docker.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+yum install docker git -y
+systemctl enable docker
+systemctl start docker
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
--- modules/instance/instance.tf
+++ modules/instance/instance.tf
@@ -15,14 +15,12 @@ data  "aws_ami" "amazon-linux-2" {
 }
 
 resource "aws_instance" "hello-instance" {
-// depends_on = ["aws_internet_gateway.gw-devops", "aws_key_pair.hello-key"]
 
  ami                         = "${data.aws_ami.amazon-linux-2.id}"
  associate_public_ip_address = true
  instance_type               = "t2.micro"
  key_name                    = "${var.private_ssh_key}"
  vpc_security_group_ids      = ["${var.security_group_id}"]
-// vpc_security_group_ids      = ["${aws_security_group.allow_ports.id}"]
  subnet_id                   = "${var.subnet_name}"
-// subnet_id                   = "${aws_subnet.public-sb.id}"
+ user_data = "${file("install_docker.sh")}"
 }
