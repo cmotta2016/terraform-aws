939338a2462f789b0db298f5de7a80986ddeeb3e
Carlos Motta <motta.carlos08@gmail.com>
2019-11-01 14:34:53 -0300

    first commit

--- modules/instance/instance.tf
+++ modules/instance/instance.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "hello-instance" {
 
  ami                         = "${data.aws_ami.amazon-linux-2.id}"
  associate_public_ip_address = true
- instance_type               = "t2.micro"
+ instance_type               = "t2.medium"
  key_name                    = "${var.private_ssh_key}"
  vpc_security_group_ids      = ["${var.security_group_id}"]
  subnet_id                   = "${var.subnet_name}"
